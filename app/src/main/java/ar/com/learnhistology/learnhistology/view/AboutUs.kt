c7d1f46fbbed93241a8ed8af7961a8512dcc2d97
Pablo Beltran <pablonqn55@gmail.com>
2021-09-05 22:32:29 -0300

    App terminada.

---
+++ app/src/main/java/ar/com/learnhistology/learnhistology/view/AboutUs.kt
@@ -0,0 +1,27 @@
+package ar.com.learnhistology.learnhistology.view
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Paint
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ar.com.learnhistology.learnhistology.R
+import ar.com.learnhistology.learnhistology.databinding.ActivityAboutUsBinding
+
+class AboutUs : AppCompatActivity() {
+    lateinit var mBinding:ActivityAboutUsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityAboutUsBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.tvClickable.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG)
+        mBinding.tvClickable.setOnClickListener {
+            val uri:Uri = Uri.parse("https://www.linkedin.com/in/pablo-beltran95/")
+            val goToUri = Intent(Intent.ACTION_VIEW,uri)
+            startActivity(goToUri)
+        }
+    }
+}
